4aade06fed5d6a02489d6f498229eda024830706
Mustafa Mustafayev <mmustafayev0001@gmail.com>
2022-01-20 15:49:31 +0400

    tested

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -71,8 +71,6 @@ namespace API.Controllers
         {
             try
             {
-                int a;
-                int b;
                 if (!ModelState.IsValid)
                 {
                     return Ok(new SuccessDataResult<Result>(Messages.InvalidModel));
